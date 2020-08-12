840cf4023fdaacc535f70be52925e3b3fa243b0d
Chris <yongwankang@hotmail.com>
2020-08-12 23:02:17 +0800

    Implement info page

--- CISnew/info/views.py
+++ CISnew/info/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Stores, Dishes
 
 
 def info(request):
-    return HttpResponse("Hello, world. You're at the info index.")
+    stores = Stores.objects.all()
+    context={'stores':stores}
+    return render(request,'info/index.html',context)
